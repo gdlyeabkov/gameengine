72c8e0c9af7178a8977b84ff70e6c2a5ffa13f02
gdlyeabkov <gdlyeabkov@gmail.com>
2021-12-12 09:58:16 +0300

    expand_hierarch_gameobjects

--- GameEngine/MainWindow.xaml.cs
+++ GameEngine/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Speech.Synthesis;
 
 namespace GameEngine
 {
@@ -20,9 +21,58 @@ namespace GameEngine
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public List<Dictionary<String, Object>> gameObjects;
+        public SpeechSynthesizer debugger;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gameObjects = new List<Dictionary<String, Object>>();
+            debugger = new SpeechSynthesizer();
+
         }
+
+        private void ToggleGameObjectHandler(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock gameObjectToggler = ((TextBlock)(sender));
+            bool isGameObjectDrop = gameObjectToggler.Text == "⮮";
+            if (isGameObjectDrop)
+            {
+                gameObjectToggler.Text = "⮫";
+            }
+            else if (!isGameObjectDrop)
+            {
+                gameObjectToggler.Text = "⮮";
+            }
+        }
+
+        private void CreateGameObjectHandler(object sender, RoutedEventArgs e)
+        {
+            Dictionary<String, Object> newGameObject = new Dictionary<String, Object>();
+            int gameObjectId = gameObjects.Count + 1;
+            newGameObject.Add("id", gameObjectId);
+            gameObjects.Add(newGameObject);
+            debugger.Speak(gameObjects.Count.ToString());
+
+            hierarchyGameObjects.Children.RemoveRange(0, hierarchyGameObjects.Children.Count);
+            foreach (Dictionary<String, Object> gameObject in gameObjects)
+            {
+                StackPanel newHierarchyGameObject = new StackPanel();
+                newHierarchyGameObject.Orientation = Orientation.Horizontal;
+                newHierarchyGameObject.Background = System.Windows.Media.Brushes.LightGray;
+                newHierarchyGameObject.Height = 35;
+                TextBlock newHierarchyGameObjectToggler = new TextBlock();
+                newHierarchyGameObjectToggler.Text = "⮫";
+                newHierarchyGameObjectToggler.Margin = new Thickness(5, 0, 5, 0);
+                TextBlock newHierarchyGameObjectLabel = new TextBlock();
+                newHierarchyGameObjectLabel.Text = "Игровой объект №" + gameObject["id"].ToString();
+                newHierarchyGameObject.Children.Add(newHierarchyGameObjectToggler);
+                newHierarchyGameObject.Children.Add(newHierarchyGameObjectLabel);
+                hierarchyGameObjects.Children.Add(newHierarchyGameObject);
+            }
+        }
+
     }
 }
